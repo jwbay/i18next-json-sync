f262184ddf859fd3fa915c759dee5334cc5dc41e
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2020-10-28 11:51:28 +0200

    Bump deps

--- src/cli.ts
+++ src/cli.ts
@@ -1,60 +1,59 @@
 #!/usr/bin/env node
 
+import * as yargs from 'yargs';
 import sync from './';
-import yargs = require('yargs');
 
-const {
-	check,
-	files,
-	primary,
-	languages,
-	space,
-	lineendings,
-	finalnewline,
-	newkeysempty
-} = yargs
-	.describe('c','Audit files in memory instead of changing them on the filesystem and throw an error if any changes would be made')
-	.alias('c','check')
-	.boolean('c')
+const { check, files, primary, languages, space, lineendings, finalnewline, newkeysempty } = yargs
+	.describe(
+		'check',
+		'Audit files in memory instead of changing them on the filesystem and throw an error if any changes would be made'
+	)
+	.alias('check', 'c')
+	.boolean('check')
 
-	.describe('f', 'Glob pattern for the resource JSON files')
+	.describe('files', 'Glob pattern for the resource JSON files')
 	.example('--files', `'**/locales/*.json'`)
-	.alias('f', 'files')
+	.alias('files', 'f')
+	.string('files')
 
-	.describe('p', 'Primary localization language. Other language files will be changed to match')
-	.alias('p', 'primary')
+	.describe(
+		'primary',
+		'Primary localization language. Other language files will be changed to match'
+	)
+	.alias('primary', 'p')
+	.string('primary')
 
-	.describe('l', `Language files to create if they don't exist`)
+	.describe('languages', `Language files to create if they don't exist`)
 	.example('--languages', 'es fr pt-BR ja')
-	.alias('l', 'languages')
-	.array('l')
+	.alias('languages', 'l')
+	.array('languages')
 
-	.describe('s', 'Space value used for JSON.stringify when writing JSON files to disk')
-	.alias('s', 'space')
+	.describe('space', 'Space value used for JSON.stringify when writing JSON files to disk')
+	.alias('space', 's')
+	.number('space')
 
-	.describe('le', 'Line endings used when writing JSON files to disk -- either LF or CRLF')
-	.alias('le', 'lineendings')
+	.describe('lineendings', 'Line endings used when writing JSON files to disk -- either LF or CRLF')
+	.alias('lineendings', 'le')
+	.string('lineendings')
 
-	.describe('fn', 'Insert a final newline when writing JSON files to disk')
-	.alias('fn', 'finalnewline')
-	.boolean('fn')
+	.describe('finalnewline', 'Insert a final newline when writing JSON files to disk')
+	.alias('finalnewline', 'fn')
+	.boolean('finalnewline')
 
-	.describe('e', 'Use empty string for new keys instead of the primary language value')
-	.alias('e', 'newkeysempty')
-	.boolean('e')
+	.describe('newkeysempty', 'Use empty string for new keys instead of the primary language value')
+	.alias('newkeysempty', 'e')
+	.boolean('newkeysempty')
 
-	.help('h')
-	.alias('h', 'help')
-
-	.argv;
+	.help('help')
+	.alias('help', 'h').argv;
 
 sync({
 	check,
 	files,
 	primary,
-	createResources: languages,
+	createResources: languages as string[],
 	space,
-	lineEndings: lineendings,
+	lineEndings: lineendings as any,
 	finalNewline: finalnewline,
 	newKeysEmpty: newkeysempty
 });
